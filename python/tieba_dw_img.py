07a23794164ac3c6a2a391f14c4624c6ec5eff72
Limingyi <laconism.lmy@gmail.com>
2013-01-31 23:54:45 +0800

    add exceptional handling

--- python/tieba_dw_img.py
+++ python/tieba_dw_img.py
@@ -9,6 +9,7 @@ from urllib.parse import urljoin
 import urllib.request
 
 tieba_name = "c语言"
+tieba_url = "http://tieba.baidu.com/"
 work_dir = os.getcwd()
 album_count = 0
 img_count = 0
@@ -21,10 +22,7 @@ while page_num > 0:
     page = urllib.request.urlopen("http://tieba.baidu.com/f/tupian?kw=%s&pn=%d" % (tieba_name,page_num))
     soup = BeautifulSoup(page.read())
     for tag in soup.find_all('a', class_="alb_box_inner"):
-        album = {
-            "name": tag["href"].split("=")[-1],
-            "href": urllib.parse.urljoin("http://tieba.baidu.com/",tag["href"]),
-            }
+        album = tag["href"]
         if album not in album_entries:
             album_entries.append(album)
         else:
@@ -35,16 +33,15 @@ while page_num > 0:
 # fetch images
 cur_page = None
 next_page = None
-page_num = 2
 img_set = set()
 for album in album_entries:
-    cur_dir = os.path.join(work_dir,urllib.parse.unquote_plus(album["name"],encoding='gbk'))
-    os.makedirs(cur_dir)
-    next_page = album["href"]
+    cur_dir = os.path.join(work_dir,urllib.parse.unquote_plus(album.split("=")[-1],encoding='gbk'))
+    try: os.makedirs(cur_dir)
+    except: continue
+    next_page = urllib.parse.urljoin(tieba_url,album)
     while (cur_page != next_page):
-        print ("%s" % next_page)
-        page = urllib.request.urlopen(next_page)
         cur_page = next_page
+        page = urllib.request.urlopen(cur_page)
         soup = BeautifulSoup(page.read())
         for image in soup.find_all("img",onload="ZyqTupian.resize(this)"):
             img_src = image["src"]
@@ -52,18 +49,17 @@ for album in album_entries:
             img_set.add(img_src)
         for tmp in soup.find_all('a'):
             if (tmp.string == "下一页"):
-                next_page = album["href"] + ("&pn=%d" % page_num)
-        page_num += 1
+                next_page = urllib.parse.urljoin(tieba_url,tmp["href"])
+    print ("album's name:%s, images num:%d" % (urllib.parse.unquote_plus(album.split("=")[-1],encoding='gbk'),len(img_set)))
+    img_count += len(img_set)
     for img_addr in img_set:
         path = os.path.join(cur_dir,img_addr.split('/')[-1])
         img_dld = urllib.request.urlopen(img_addr).read()
         file_tmp = open(path, 'wb')
         file_tmp.write(img_dld)
         file_tmp.close()
-    page_num = 2
     album_count += 1
     img_count += len(img_set)
-    print ("%s download complete, totally %d albums, %d images" % (urllib.parse.unquote_plus(album["name"],encoding='gbk'),album_count,len(img_set)))
     img_set.clear()
 
 print ("download complete, totally %d albums, %d images")
